8a104d41b38f194ab567235c191ad68f49cedc59
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-06-08 21:17:03 -0500

    backup

--- HighlightLinksInCategory.php
+++ HighlightLinksInCategory.php
@@ -28,23 +28,40 @@ class HighlightLinksInCategory {
         # linkcolour_ids only contains pages that exist, which does a lot
         # of our work for us
         # let's follow all redirects if the user wants to
-        $targetPageIDs  = array_keys($linkcolour_ids);
+        
+        $pagesToQuery = array_keys($linkcolour_ids);
+        $nonRedirects = array_keys($linkcolour_ids);
+        $pageToTargetLookup = [];
+
         $dbr = wfGetDB( DB_REPLICA );
         
         if ( $wgHighlightLinksInCategoryFollowRedirects ) {
             $res0 = $dbr->select(
                 [ 'redirect', 'page' ],
-                [ 'rd_from', 'page_id' ],
+                array('rd_from', 'page_id'),
                 $dbr->makeList( [
                     'rd_namespace = page_namespace',
                     'rd_title = page_title',
-                    $dbr->makeList( $targetPageIDs, LIST_OR ),
-                   'rd_interwiki IS NULL',
+                    $dbr->makeList(
+                        array('rd_from' => $pagesToQuery ), LIST_OR ),
+                   // 'rd_interwiki IS NULL',
                 ], LIST_AND )
             );
             foreach ( $res0 as $row ) {
-                $targetPageIDs = array_diff( $targetPageIDs, $row->rd_from );
-                $targetPageIDs[] = $row->page_id;
+                # first, forget this page as a non-redirect
+                $nonRedirects = array_diff( $nonRedirects, [$row->rd_from] );
+                # and also as a page to query
+                $pagesToQuery = array_diff( $pagesToQuery, [$row->rd_from] );
+                
+                # then make sure we remember this association
+                $pageToTargetLookup[$row->rd_from] = $row->page_id;
+                # and we also need to query this later
+                $pagesToQuery[] = $row->page_id;
+            }
+            
+            # now that nonRedirects is fully populated, tell our lookup about them
+            foreach ( $nonRedirects as $nonRedirect ) {
+                $pageToTargetLookup[$nonRedirect] = $nonRedirect;
             }
         }
 
@@ -57,18 +74,30 @@ class HighlightLinksInCategory {
             array('cl_from', 'cl_to'),
             $dbr->makeList( array(
                 $dbr->makeList(
-                    array( 'cl_from' => $targetPageIDs ), LIST_OR),
+                    array( 'cl_from' => $pagesToQuery ), LIST_OR),
                 $dbr->makeList(
                     array( 'cl_to'   => $catNames), LIST_OR)
                 ),
                 LIST_AND
             )
         );
-
+        
+        $classes = [];
+        foreach( $res as $s ) {
+            $classes[ $s->cl_from ] = '';
+            $classes[ $s->cl_from ] .= ' ' . $wgHighlightLinksInCategory[ $s->cl_to ];
+        }
+        
         # Add the color classes to each page
-        foreach ( $res as $s ) {
-            $colours[ $linkcolour_ids[ $s->cl_from ] ]
-                .= ' ' . $wgHighlightLinksInCategory[ $s->cl_to ];
+        foreach ( $pageToTargetLookup as $page=>$target ) {
+            echo( $page );
+            echo( ',' );
+            echo( $target );
+            echo( $classes[ $target ] );
+            echo( '<br>' );
+            if ( $classes[ $target ] != '' ) {
+                $colours[ $page ] = $classes[ $target ];
+            }
         }
     }
 
